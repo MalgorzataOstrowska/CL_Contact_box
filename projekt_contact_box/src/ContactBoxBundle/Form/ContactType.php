eaef8d694c205daae3bfd3c15b10d05b0735a92d
Malgorzata Ostrowska <ostrowska.malgorzata.ewa@gmail.com>
2017-02-17 23:40:41 +0100

    Generation of a form for Contact entity

---
+++ projekt_contact_box/src/ContactBoxBundle/Form/ContactType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ContactBoxBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('description')
+        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ContactBoxBundle\Entity\Contact'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'contactboxbundle_contact';
+    }
+
+
+}
