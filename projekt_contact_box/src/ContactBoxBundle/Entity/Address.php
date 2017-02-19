8667ef4ded7f36252a501ccb8fa1f84abc61276b
Malgorzata Ostrowska <ostrowska.malgorzata.ewa@gmail.com>
2017-02-19 17:01:27 +0100

    Creation of a new entity: Address

---
+++ projekt_contact_box/src/ContactBoxBundle/Entity/Address.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace ContactBoxBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Address
+ *
+ * @ORM\Table(name="address")
+ * @ORM\Entity(repositoryClass="ContactBoxBundle\Repository\AddressRepository")
+ */
+class Address
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=100)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="street", type="string", length=200)
+     */
+    private $street;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="houseNumber", type="string", length=100, nullable=true)
+     */
+    private $houseNumber;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apartmentNumber", type="string", length=100, nullable=true)
+     */
+    private $apartmentNumber;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return Address
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set street
+     *
+     * @param string $street
+     * @return Address
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    /**
+     * Get street
+     *
+     * @return string 
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set houseNumber
+     *
+     * @param string $houseNumber
+     * @return Address
+     */
+    public function setHouseNumber($houseNumber)
+    {
+        $this->houseNumber = $houseNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get houseNumber
+     *
+     * @return string 
+     */
+    public function getHouseNumber()
+    {
+        return $this->houseNumber;
+    }
+
+    /**
+     * Set apartmentNumber
+     *
+     * @param string $apartmentNumber
+     * @return Address
+     */
+    public function setApartmentNumber($apartmentNumber)
+    {
+        $this->apartmentNumber = $apartmentNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get apartmentNumber
+     *
+     * @return string 
+     */
+    public function getApartmentNumber()
+    {
+        return $this->apartmentNumber;
+    }
+}
